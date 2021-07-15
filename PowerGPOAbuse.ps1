38474edc20b1f130c2a93a386d307243ed3f7ef7
armysick <uh4v3b33nh4ck3d@gmail.com>
2021-07-15 19:18:04 +0100

    version back to 1

--- PowerGPOAbuse.ps1
+++ PowerGPOAbuse.ps1
@@ -1765,7 +1765,7 @@ function Set-DomainObjectProperty {
                 Write-Verbose "[Set-DomainObjectProperty] Setting '$property' for $ObjectType $($Object.cn)"
                 if ($property -eq "versionNumber"){
                   Write-Verbose "[DEBUG] - Changing $($SET[$property]) to 2 on '$property' -- TYPE $($SET[$property].GetType())"
-                  $ADSIObject.Put($property, 2)
+                  $ADSIObject.Put($property, 1)
                 } else {
                     $ADSIObject.Put($property, $SET[$property]) 
                 }
